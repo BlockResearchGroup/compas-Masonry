2c3c361f91531dacc88a959fe3fe232623ddd1f3
tomvanmele <tom.v.mele@gmail.com>
2024-09-11 10:55:25 +0200

    update reqs

--- plugin/Masonry_reqs_install.py
+++ plugin/Masonry_reqs_install.py
@@ -1,9 +1,10 @@
 #! python3
-# r: compas compas_ags compas_fd compas_rui compas_session compas_tna
+# r: compas compas_assembly compas_model compas_rui compas_session
 
 import compas  # noqa: F401
 import compas_assembly  # noqa: F401
-import compas_cra  # noqa: F401
+
+# import compas_cra  # noqa: F401
 import compas_model  # noqa: F401
 import compas_rui  # noqa: F401
 import compas_session  # noqa: F401
